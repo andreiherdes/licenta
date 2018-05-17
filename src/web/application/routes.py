729284ed4547f88238dba59efd944fe623626a4d
Andrei Herdes <andrei.herdes@info.uaic.ro>
2018-05-17 23:23:40 +0300

    Added live stream + face and emotion detection support

--- src/web/application/routes.py
+++ src/web/application/routes.py
@@ -1,6 +1,26 @@
 from application import app
+from flask import render_template, Response
+from camera import VideoCamera
 
-@app.route('/')
+emotion = 'None'
+
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    return render_template('index.html')
+
+def gen(camera):
+    global emotion
+    while True:
+        frame, emotion_text = camera.get_frame
+        emotion = emotion_text
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+
+@app.route('/emotion_text')
+def get_emotion():
+    return Response(emotion, mimetype='text/xml')
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen(VideoCamera()),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
--- src/web/server.py
+++ src/web/server.py
@@ -1,8 +1,12 @@
-"""
-This script runs the Homework_4 application using a development server.
-"""
-
+#
+# Date: 16/05/2018
+# Description:
+# Modified to support face detection in the video stream using opencv and detect
+# emotions using a model trained with keras.
+# Video/Webcam stream credits go to Miguel Grinberg and ckieric. Thanks!
+# Credits: http://blog.miguelgrinberg.com/post/video-streaming-with-flask
+#          https://github.com/log0/video_streaming_with_flask_example
 from application import app
 
 if __name__ == '__main__':
-    app.run('localhost', 8080)
+    app.run('localhost', 8080, debug=True)
