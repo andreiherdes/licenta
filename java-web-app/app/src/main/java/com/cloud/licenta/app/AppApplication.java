2f1fe22901743ec0f07dbfbf4a42f3713b0576dc
Andrei Herdes <andrei.herdes@info.uaic.ro>
2018-06-22 19:55:13 +0300

    Removed old python project and added spring boot basic web app

---
+++ java-web-app/app/src/main/java/com/cloud/licenta/app/AppApplication.java
@@ -0,0 +1,12 @@
+package com.cloud.licenta.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppApplication.class, args);
+	}
+}
